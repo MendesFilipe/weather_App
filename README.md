f444ac55b217d42215752b27b5434d100c00acf2
Filipe Mendes <mendes.m.filipe@gmail.com>
2020-09-10 19:53:30 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+### Aplicativo de clima :snowflake:
+
+Aplicativo de clima usando ReactJS e a Weather API
+
+Link do projeto: 
+
+## Para começar :arrow_forward:
+
+- Para começar, você pode fazer o download do projeto. Ou clonar o projeto com o seguinte comando:
+
+`git clone https://github.com/MendesFilipe/weather_app.git`
+
+- Agora entre na pasta criada pelo git com:
+
+`cd weather_app`
+
+- Depois, instale o projeto com o seguinte comando:
+
+`npm install`
+
+## Para desenvolver :minidisc:
+
+Há um servidor de desenvolvimento, uma suite de testes com [jest](https://github.com/facebook/jest) 
+
+- Para inicializar, o comando é:
+
+`npm start`
+
+- Para testar:
+
+`npm test`
+
+## Para compilar :floppy_disk:
+
+Para compilar a aplicação basta seguir os dois passos abaixo:
+
+- Para realizar o build da aplicação:
+
+`npm build`
+
+
+## Construído com :construction:
+
+- [React](https://reactjs.org/) 
+- [Jest](https://jestjs.io/) 
+- [ESLint](https://eslint.org/) 
+
+## Licença :page_with_curl:
+
+Esse projeto possui a licença **MIT**.
---
+++ src/app_component/form.component.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./form.style.css";
+
+const Form = props => {
+  return (
+    <div className="container h-100">
+      <form onSubmit={props.loadweather}>
+        <div>{props.error ? error() : ""}</div>
+        <div className="row">
+          <div className="col-md-3 offset-md-2">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Cidade"
+              name="city"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-md-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="País"
+              name="country"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-md-3 mt-md-0 mt-2 text-md-left ">
+            <button className="btn btn-warning">Obter clima</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const error = props => {
+  return (
+    <div className="alert alert-danger mx-5" role="alert">
+      Por favor digite a cidade e o país...!
+    </div>
+  );
+};
+
+export default Form;
